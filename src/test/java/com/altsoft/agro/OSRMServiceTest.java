38f591ee638f51a3d4b191f79879c202f7c3960a
Andrey Bogdanov <abogdanov37@gmail.com>
2018-12-18 12:39:06 +0300

    Some fixes

--- src/test/java/com/altsoft/agro/OSRMServiceTest.java
+++ src/test/java/com/altsoft/agro/OSRMServiceTest.java
@@ -5,6 +5,9 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -37,6 +40,9 @@ public class OSRMServiceTest {
         HttpResponse response = client.execute(request);
         //Assert
         assertThat(response.getStatusLine().getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+        String json_string = EntityUtils.toString(response.getEntity());
+        Object temp1 = JSONValue.parse(json_string);
+        System.out.println(json_string);
         // Добавить проверки на содержимое ответа если необходимо
     }
 
